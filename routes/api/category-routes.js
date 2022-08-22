f5324e93e79d9e14ba1f41ca56d9227c0c0f1e40
Patrick Gu <patrickthegu@gmail.com>
2022-08-22 16:13:42 +1000

    fixed models, routes all working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findALL({
+    const categoryData = await Category.findAll({
       include: [{model: Product}],
     });
     res.status(200).json(categoryData)
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,8 +8,8 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product, through: ProductTag, as: 'products' }],
-    })
+      include: [{model: Product, through: ProductTag, as: 'products'}],
+    });
     res.status(200).json(tagData)
   } catch (err) {
     res.status(400).json(err);
@@ -21,8 +21,8 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk( req.params.id, {
-      include: [{ model: Product, through: ProductTag, as: 'products' }]
-      })
+      include: [{model: Product, through: ProductTag, as: 'products'}]
+      });
 
       if (!tagData) {
         res.status(404).json({ message: 'No tag matches this id' });
